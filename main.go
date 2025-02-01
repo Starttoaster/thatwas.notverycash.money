02733ac42f22161bafbb967a4cc641fa4b97bfd0
Starttoaster <b.butler@chia.net>
2025-02-01 11:58:16 -0800

    Add request logging (#5)
    * Add request logging
    
    * Remove redundant return

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"embed"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 )
@@ -15,22 +17,102 @@ import (
 //go:embed robots.txt
 var static embed.FS
 
+var logger *slog.Logger
+
 func main() {
-	http.HandleFunc("/", maxBytesMiddle(pathCheckMiddle(allowedMethodsMiddle(commonHeadersMiddle(indexHandler)))))
-	http.HandleFunc("/ofyou", maxBytesMiddle(pathCheckMiddle(allowedMethodsMiddle(commonHeadersMiddle(indexHandler)))))
-	http.HandleFunc("/image.avif", maxBytesMiddle(pathCheckMiddle(allowedMethodsMiddle(commonHeadersMiddle(imageHandler)))))
-	http.HandleFunc("/image-small.avif", maxBytesMiddle(pathCheckMiddle(allowedMethodsMiddle(commonHeadersMiddle(smallImageHandler)))))
-	http.HandleFunc("/robots.txt", maxBytesMiddle(pathCheckMiddle(allowedMethodsMiddle(commonHeadersMiddle(robotsHandler)))))
+	logger = slog.New(
+		slog.NewTextHandler(
+			os.Stdout,
+			&slog.HandlerOptions{
+				Level: slog.LevelInfo,
+			},
+		),
+	)
+
+	http.HandleFunc("/",
+		maxBytesMiddle(
+			pathCheckMiddle(
+				allowedMethodsMiddle(
+					logMiddle(
+						commonHeadersMiddle(indexHandler))))))
+	http.HandleFunc("/ofyou",
+		maxBytesMiddle(
+			pathCheckMiddle(
+				allowedMethodsMiddle(
+					logMiddle(
+						commonHeadersMiddle(indexHandler))))))
+	http.HandleFunc("/image.avif",
+		maxBytesMiddle(
+			pathCheckMiddle(
+				allowedMethodsMiddle(
+					logMiddle(
+						commonHeadersMiddle(imageHandler))))))
+	http.HandleFunc("/image-small.avif",
+		maxBytesMiddle(
+			pathCheckMiddle(
+				allowedMethodsMiddle(
+					logMiddle(
+						commonHeadersMiddle(smallImageHandler))))))
+	http.HandleFunc("/robots.txt",
+		maxBytesMiddle(
+			pathCheckMiddle(
+				allowedMethodsMiddle(
+					logMiddle(
+						commonHeadersMiddle(robotsHandler))))))
 
 	srv := &http.Server{
-		Addr:              fmt.Sprintf(":%d", 8080),
+		Addr:              fmt.Sprintf("%s:%d", "0.0.0.0", 8080),
 		ReadHeaderTimeout: 5 * time.Second,
 		ReadTimeout:       15 * time.Second,
 		WriteTimeout:      15 * time.Second,
 		IdleTimeout:       15 * time.Second,
 	}
-	log.Printf("Server listening on %s...\n", srv.Addr)
+	logger.Info("starting server", "address", srv.Addr)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func buildLogArgs(r *http.Request) []any {
+	args := []any{
+		"remote", r.RemoteAddr,
+		"method", r.Method,
+		"path", r.URL.Path,
+		"user-agent", r.UserAgent(),
+	}
+
+	if xff := r.Header.Get("x-forwarded-for"); xff != "" {
+		args = append(args, "x-forwarded-for", xff)
+	}
+
+	if realIP := r.Header.Get("x-real-ip"); realIP != "" {
+		args = append(args, "x-real-ip", realIP)
+	}
+
+	return args
+}
+
+func logMiddle(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger.Info("handling new request", buildLogArgs(r)...)
+		next.ServeHTTP(w, r)
+	}
+}
+
+type handleErrorOptions struct {
+	Error      error
+	LogMsg     string
+	PublicMsg  string
+	StatusCode int
+}
+
+func handleError(w http.ResponseWriter, r *http.Request, opts handleErrorOptions) {
+	args := buildLogArgs(r)
+	if opts.Error != nil {
+		args = append(args, "error", opts.Error.Error())
+		logger.Error(opts.LogMsg, args...)
+	} else {
+		logger.Info(opts.LogMsg, args...)
+	}
+	http.Error(w, opts.PublicMsg, opts.StatusCode)
 }
 
 func maxBytesMiddle(next http.HandlerFunc) http.HandlerFunc {
@@ -43,7 +125,11 @@ func maxBytesMiddle(next http.HandlerFunc) http.HandlerFunc {
 func pathCheckMiddle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if strings.Contains(r.URL.Path, "..") {
-			http.Error(w, "Invalid path", http.StatusBadRequest)
+			handleError(w, r, handleErrorOptions{
+				LogMsg:     "blocked - invalid path",
+				PublicMsg:  "Invalid path",
+				StatusCode: http.StatusBadRequest,
+			})
 			return
 		}
 		next.ServeHTTP(w, r)
@@ -53,7 +139,11 @@ func pathCheckMiddle(next http.HandlerFunc) http.HandlerFunc {
 func allowedMethodsMiddle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
-			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			handleError(w, r, handleErrorOptions{
+				LogMsg:     "blocked - method not allowed",
+				PublicMsg:  "Method Not Allowed",
+				StatusCode: http.StatusMethodNotAllowed,
+			})
 			return
 		}
 		next.ServeHTTP(w, r)
@@ -71,40 +161,65 @@ func commonHeadersMiddle(next http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
-func indexHandler(w http.ResponseWriter, _ *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	content, err := static.ReadFile("index.html")
 	if err != nil {
-		http.Error(w, "Error loading page", http.StatusInternalServerError)
+		handleError(w, r, handleErrorOptions{
+			Error:      err,
+			LogMsg:     "loading from static content",
+			PublicMsg:  "Error loading page",
+			StatusCode: http.StatusInternalServerError,
+		})
 		return
 	}
 
 	w.Header().Set("Content-Type", "text/html")
 	_, err = w.Write(content)
 	if err != nil {
-		http.Error(w, "Error loading page", http.StatusInternalServerError)
+		handleError(w, r, handleErrorOptions{
+			Error:      err,
+			LogMsg:     "writing to response writer",
+			PublicMsg:  "Error loading page",
+			StatusCode: http.StatusInternalServerError,
+		})
 		return
 	}
 }
 
-func robotsHandler(w http.ResponseWriter, _ *http.Request) {
+func robotsHandler(w http.ResponseWriter, r *http.Request) {
 	content, err := static.ReadFile("robots.txt")
 	if err != nil {
-		http.Error(w, "Error loading page", http.StatusInternalServerError)
+		handleError(w, r, handleErrorOptions{
+			Error:      err,
+			LogMsg:     "loading from static content",
+			PublicMsg:  "Error loading page",
+			StatusCode: http.StatusInternalServerError,
+		})
 		return
 	}
 
 	w.Header().Set("Content-Type", "text/plain")
 	_, err = w.Write(content)
 	if err != nil {
-		http.Error(w, "Error loading page", http.StatusInternalServerError)
+		handleError(w, r, handleErrorOptions{
+			Error:      err,
+			LogMsg:     "writing to response writer",
+			PublicMsg:  "Error loading page",
+			StatusCode: http.StatusInternalServerError,
+		})
 		return
 	}
 }
 
-func imageHandler(w http.ResponseWriter, _ *http.Request) {
+func imageHandler(w http.ResponseWriter, r *http.Request) {
 	content, err := static.ReadFile("cash.avif")
 	if err != nil {
-		http.Error(w, "Error loading image", http.StatusInternalServerError)
+		handleError(w, r, handleErrorOptions{
+			Error:      err,
+			LogMsg:     "loading from static content",
+			PublicMsg:  "Error loading page",
+			StatusCode: http.StatusInternalServerError,
+		})
 		return
 	}
 
@@ -112,15 +227,25 @@ func imageHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Cache-Control", "public, max-age=31536000")
 	_, err = w.Write(content)
 	if err != nil {
-		http.Error(w, "Error loading page", http.StatusInternalServerError)
+		handleError(w, r, handleErrorOptions{
+			Error:      err,
+			LogMsg:     "writing to response writer",
+			PublicMsg:  "Error loading page",
+			StatusCode: http.StatusInternalServerError,
+		})
 		return
 	}
 }
 
-func smallImageHandler(w http.ResponseWriter, _ *http.Request) {
+func smallImageHandler(w http.ResponseWriter, r *http.Request) {
 	content, err := static.ReadFile("cash-small.avif")
 	if err != nil {
-		http.Error(w, "Error loading image", http.StatusInternalServerError)
+		handleError(w, r, handleErrorOptions{
+			Error:      err,
+			LogMsg:     "loading from static content",
+			PublicMsg:  "Error loading page",
+			StatusCode: http.StatusInternalServerError,
+		})
 		return
 	}
 
@@ -128,7 +253,12 @@ func smallImageHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Cache-Control", "public, max-age=31536000")
 	_, err = w.Write(content)
 	if err != nil {
-		http.Error(w, "Error loading page", http.StatusInternalServerError)
+		handleError(w, r, handleErrorOptions{
+			Error:      err,
+			LogMsg:     "writing to response writer",
+			PublicMsg:  "Error loading page",
+			StatusCode: http.StatusInternalServerError,
+		})
 		return
 	}
 }
